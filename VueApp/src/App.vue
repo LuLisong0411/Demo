2e71aad4af483ad2913a42e6965203de2069ff2f
LuLisong0411 <951170572@qq.com>
2018-08-13 22:40:30 +0800

    vueapp

---
+++ VueApp/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <CommonHeader>
+
+    </CommonHeader>
+    <router-view/>
+    <CommonFooter></CommonFooter>
+  </div>
+</template>
+import CommonHeader from '@/components/CommonHeader'
+<script>
+export default {
+  components: {
+    CommonHeader
+   },
+};
+</script>
+
